06e86f9bc1846893016c10b731d9dceb8915d3ff
Virginia Vieira <152438496+VirVieira@users.noreply.github.com>
2024-12-17 17:50:49 +0000

    -Clean Code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.tips_calculate"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.tips_calculate"

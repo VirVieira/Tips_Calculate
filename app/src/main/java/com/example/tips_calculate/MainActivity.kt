cde807c61d538d61bed9fad7a6efd6f564a32ced
Virginia Vieira <152438496+VirVieira@users.noreply.github.com>
2024-12-17 19:42:23 +0000

    - Atualização do código - Snackbar - Interagindo com radio Group

--- app/src/main/java/com/example/tips_calculate/MainActivity.kt
+++ app/src/main/java/com/example/tips_calculate/MainActivity.kt
@@ -1,40 +1,67 @@
-package com.example.tip
+package com.example.tips_calculate
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tips_calculate.databinding.ActivityMainBinding
-import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
-   private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        enableEdgeToEdge()
         setContentView(binding.root)
 
-        val total: Float = binding.tieTotal.text.toString().toFloat()
-        val npeople: Float = binding.tieNumPeople.text.toString().toFloat()
-
+        var percentage: Int = 0
         binding.rbOptionOne.setOnCheckedChangeListener { _, isChecked ->
-            println("Virginia Option one: $isChecked")
+            if (isChecked) {
+                percentage = 10
+            }
         }
         binding.rbOptionTwo.setOnCheckedChangeListener { _, isChecked ->
-            println("Virginia Option two: $isChecked")
-        }
-        binding.rbRboptionThree.setOnCheckedChangeListener { _, isChecked ->
-            println("Virginia Option Three: $isChecked")
-        }
+            if (isChecked) {
+                percentage = 15
+            }
+
+            binding.rbRboptionThree.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    percentage = 20
+                }
+            }
+
+            binding.btnDone.setOnClickListener {
+                val totalTableTemp = binding.tieTotal.text
+                val nPeopleTemp = binding.tieNumPeople.text
+
+                if (totalTableTemp!!.isEmpty() == true || nPeopleTemp!!.isEmpty() == true) {
+                    Snackbar.make(binding.tieTotal, "Preencha todos os Campos", Snackbar.LENGTH_LONG)
+                        .show()
+
+                } else {
+
+                }
+
+                val totalTable: Float = totalTableTemp.toString().toFloat()
+                val npeople: Int = nPeopleTemp.toString().toInt()
+
+                val totalTemp = totalTable / npeople
+                val tips = totalTemp * percentage / 100
+                val totalWithTips = totalTemp + tips
+                binding.tvResult.text = "Total with tips: $totalWithTips"
+            }
 
 
-        binding.btnClean.setOnClickListener {
-            println("Virginia1" + binding.tieTotal.text)
-            println("Virginia1" + binding.tieNumPeople.text)
+            binding.btnClean.setOnClickListener {
+                binding.tvResult.text = ""
+                binding.tieTotal.setText("")
+                binding.tieNumPeople.setText("")
+                binding.rbOptionOne.isChecked = false
+                binding.rbOptionTwo.isChecked = false
+                binding.rbRboptionThree.isChecked = false
 
+            }
         }
     }
-
 }
